9c99ceb5656ad78bb4f6e3491889a2cff9878221
Matthew Bartl <mattbartl@gmail.com>
2019-03-18 01:45:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 == 0
+def fizzbuzz(Number)
+  if Number % 5 == 0 && Number % 3 == 0
     return "FizzBuzz"
-  elsif number % 3 == 0
+  elsif Number % 3 == 0
     return "Fizz"
-  elsif number % 5 == 0
+  elsif Number % 5 == 0
     return "Buzz"
   else
     return nil
